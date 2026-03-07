c4f4cb56d6ad908f3ab09b5e7575d7a90a2583f8
Sla1mer <slameryt4@gmail.com>
2026-03-07 22:28:04 +0300

    test

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,6 +1,7 @@
 package ru.interlinkstudio
 
 import io.ktor.http.*
+import io.ktor.http.ContentDisposition.Companion.File
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
@@ -11,12 +12,15 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.launch
 import ru.interlinkstudio.Model.ApplicationRequest
+import java.io.File
 
 fun Application.configureRouting() {
     val telegram = TelegramService()
 
     routing {
-        staticResources("/images", "static/images")
+//        staticResources("/images", "static/images")
+        staticFiles("/images", File("/app/images"))
+
 
         get("/health") {
             call.respondText("OK")
